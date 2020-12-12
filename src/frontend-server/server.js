3a028241aa7e18bb8fd3d5daade84df064877f30
Raphael Kienhöfer <commit@oheoh.de>
2020-12-12 22:12:31 +0100

    Remove obsolete code

--- src/frontend-server/server.js
+++ src/frontend-server/server.js
@@ -17,29 +17,7 @@ const app = module.exports = express();
 const dirname = fs.realpathSync('./');
 const wwwroot = __dirname + '/wwwroot';
 
-// Locales might be solved by angular-localize
-//var locales = JSON.parse(fs.readFileSync(__dirname + '/locales.json', 'utf8'));
-
 var settings = JSON.parse(fs.readFileSync(dirname + '/data/user-settings.json', 'utf8'));
-
-// Locales might be solved by angular-localize
-/*
-app.get('/locales', (req, res) => {
-  console.log('[frontend] [load] [locales] index');
-  res.status(200).end(JSON.stringify(locales));
-});
-
-app.get('/locales/:localeId', (req, res) => {
-  let localeId = req.params.localeId;
-  if (localeId === undefined || localeId === '') {
-    console.log('[frontend] [FAIL] [locales] ' + localeId);
-    res.status(404).end('Language pack not found');
-  } else {
-    console.log('[frontend] [load] [locales] ' + localeId);
-    res.status(200).sendFile(__dirname + '/locales/' + localeId + '.json');
-  }
-});
-*/
 
 app.get('/settings', (req, res) => {
   const config = {
