99511240150f05d421982e713dee9561c25c38ef
Raphael Kienhöfer <commit@oheoh.de>
2021-01-13 02:31:09 +0100

    feat(api): add several REST endpoints for statistics

--- src/api/api.js
+++ src/api/api.js
@@ -569,6 +569,59 @@ api.post('/logout', function (req, res, next) {
 	res.sendStatus(200);
 });
 
+// Admin dashboard statistics
+api.get('/stats/orders', adminAccess(function (req, res, next) {
+  let orderCount = 0;
+  const stmt = prepare('SELECT COUNT(*) FROM History;')
+  stmt.get(function (err, result) {
+    if (err) { return catchDBerror(stmt, req, next, err); }
+    res.status(200).json(result['COUNT(*)']);
+  })
+}))
+
+api.get('/stats/users', adminAccess(function (req, res, next) {
+  let orderCount = 0;
+  const stmt = prepare('SELECT COUNT(*) FROM Users;')
+  stmt.get(function (err, result) {
+    if (err) { return catchDBerror(stmt, req, next, err); }
+    res.status(200).json(result['COUNT(*)']);
+  })
+}))
+
+api.get('/stats/beverages', adminAccess(function (req, res, next) {
+  const stmt = prepare('SELECT COUNT(*) FROM Beverages;')
+  stmt.get(function (err, result) {
+    if (err) { return catchDBerror(stmt, req, next, err); }
+    res.status(200).json(result['COUNT(*)']);
+  })
+}))
+
+api.get('/stats/top/beverages', adminAccess(function (req, res, next) {
+  const stmt = prepare('SELECT name, stock, price FROM Beverages WHERE name IN (SELECT beverage FROM (SELECT beverage, COUNT(beverage) AS count FROM History GROUP BY beverage ORDER BY count DESC LIMIT 5));')
+  stmt.all(
+    function (err, result) {
+      if (err) { return catchDBerror(stmt, req, next, err); }
+      res.status(200).json(result)
+    })
+}))
+
+api.get('/stats/top/savers', adminAccess(function (req, res, next) {
+  const stmt = prepare('SELECT name, balance, hidden FROM Users ORDER BY balance DESC LIMIT 5;')
+  stmt.all(function (err, result) {
+    if (err) { return catchDBerror(stmt, req, next, err); }
+    res.status(200).json(result);
+  })
+}))
+
+api.get('/stats/top/debtors', adminAccess(function (req, res, next) {
+  const stmt = prepare('SELECT name, balance, hidden FROM Users ORDER BY balance ASC LIMIT 5;')
+  stmt.all(function (err, result) {
+    if (err) { return catchDBerror(stmt, req, next, err); }
+    res.status(200).json(result);
+  })
+}))
+
+
 // Error Handling Middleware
 api.use(function (err, req, res, next) {
 	console.error(err.stack);
