0f1da3c80b67017880bdcde22a3df846827ee639
Raphael Kienhöfer <commit@oheoh.de>
2021-01-14 15:52:29 +0100

    refactor(frontend/AdminPage): move template and styles into separate files

--- src/frontend/src/app/admin/admin-page.component.ts
+++ src/frontend/src/app/admin/admin-page.component.ts
@@ -4,75 +4,8 @@ import {faBox, faHome} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-page',
-  template: `
-    <div class="row mx-0">
-      <div class="col-md-2 px-0 bg-light text-dark admin-module-selector border-right">
-        <h1 class="h4 pl-3 my-2">Admin Area</h1>
-        <ul class="list-unstyled">
-          <ng-container *ngFor="let module of modules">
-            <li *ngIf="!module.spacer; else spacer" class="admin-module" [class.admin-module-active]="activeModule===module.id"
-                [routerLink]="'../' + module.id">
-              <fa-icon *ngIf="module.icon" [icon]="module.icon"></fa-icon>
-              {{module.displayName}}
-            </li>
-            <ng-template #spacer>
-              <li class="admin-module spacer">
-                &#8203;
-              </li>
-            </ng-template>
-          </ng-container>
-        </ul>
-      </div>
-      <div class="col">
-        <app-admin-dashboard *ngIf="activeModule==='dashboard'"></app-admin-dashboard>
-      </div>
-    </div>
-  `,
-  styles: [`
-    .admin-module-selector {
-      min-height: 94vh;
-      border-color: #eee;
-    }
-
-    .admin-module {
-      padding: 8px 16px;
-      cursor: pointer;
-      transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out;
-    }
-
-    .admin-module:hover {
-      color: black;
-      background-color: rgba(0, 0, 0, .05);
-    }
-
-    .admin-module.spacer {
-      cursor: default;
-      transition: unset;
-    }
-
-    .admin-module.spacer:hover {
-      cursor: inherit;
-      background-color: inherit;
-    }
-
-    .admin-module-active {
-      background-color: #ffc107; /* bootstrap warning color */
-      color: black;
-      cursor: default;
-    }
-
-    .admin-module-active:hover {
-      background-color: #ffc107; /* bootstrap warning color */
-    }
-
-    /* remove fill on smaller screens where cols wrap
-       equal to bootstrap md breakpoint */
-    @media (max-width: 768px) {
-      .admin-module-selector {
-        min-height: unset;
-      }
-    }
-  `]
+  templateUrl: './admin-page.component.html',
+  styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
 
