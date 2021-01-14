5f7c486c401761f8a2ba805cdf6efd1ffeb57615
Raphael Kienhöfer <commit@oheoh.de>
2021-01-14 20:36:23 +0100

    refactor(frontend/AdminPage): admin module pages are now dynamically injected instead of hiding with *ngIf

--- src/frontend/src/app/admin/admin-page.component.ts
+++ src/frontend/src/app/admin/admin-page.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Type} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {faBox, faHome} from '@fortawesome/free-solid-svg-icons';
+import {AdminPageModule} from './admin-page-module';
+import {AdminDashboardComponent} from './admin-dashboard/admin-dashboard.component';
 
 @Component({
   selector: 'app-admin-page',
@@ -10,20 +12,21 @@ import {faBox, faHome} from '@fortawesome/free-solid-svg-icons';
 export class AdminPageComponent implements OnInit {
 
   public activeModule: string | undefined;
+  public activeModuleComponent: Type<any> | undefined;
 
-  public modules = [
+  public modules: AdminPageModule[] = [
     {
       id: 'dashboard',
       displayName: 'Dashboard',
-      icon: faHome
-    },
-    {
-      spacer: true,
+      icon: faHome,
+      spacerAfter: true,
+      component: AdminDashboardComponent,
     },
     {
       id: 'other',
       displayName: 'Other',
       icon: faBox,
+      spacerAfter: false,
     },
   ];
 
@@ -35,6 +38,9 @@ export class AdminPageComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.activeModule = params.module;
+      this.activeModuleComponent = this.modules.find(module => {
+        return module.id === this.activeModule;
+      })?.component;
     });
   }
 
