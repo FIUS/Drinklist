4c193bfd338dd969d437fe01c3dbff2c8a9e0572
Raphael Kienhöfer <commit@oheoh.de>
2021-01-18 20:01:12 +0100

    fix(api): top beverages now have the correct sorting

--- src/api/api.js
+++ src/api/api.js
@@ -597,7 +597,7 @@ api.get('/stats/beverages', adminAccess(function (req, res, next) {
 }))
 
 api.get('/stats/top/beverages', adminAccess(function (req, res, next) {
-  const stmt = prepare('SELECT name, stock, price FROM Beverages WHERE name IN (SELECT beverage FROM (SELECT beverage, COUNT(beverage) AS count FROM History GROUP BY beverage ORDER BY count DESC LIMIT 5));')
+  const stmt = prepare('SELECT b.name, b.stock, b.price FROM (SELECT beverage, COUNT(beverage) AS count FROM History GROUP BY beverage ORDER BY count DESC LIMIT 5) t INNER JOIN Beverages b ON t.beverage = b.name;')
   stmt.all(
     function (err, result) {
       if (err) { return catchDBerror(stmt, req, next, err); }
