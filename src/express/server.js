32c4f2de83045b58cac9c58af680b9c1730905d2
Raphael Kienhöfer <commit@oheoh.de>
2021-02-01 18:01:29 +0100

    refactor: restructure repo for api rewrite

--- src/express/server.js
+++ src/express/server.js
@@ -10,32 +10,35 @@
 /* jslint esversion: 6 */
 
 // Imports
+
 const fs = require('fs');
 
 //Check if all data files exist. If not create them. (This is required for docker.)
 require('./dataHelper.js').checkAndCreateFiles();
 
 // Import all software components
-const api       = require('./legacy/api/api.js');
-const frontend  = require('./frontend-server/server.js')
+const api = require('./legacy/api/api.js');
+const frontend = require('./frontend-server/server.js')
 
 // Settings file
 var settings = JSON.parse(fs.readFileSync(fs.realpathSync('./') + '/data/settings.json', 'utf8'));
 
 // Setup and start the api server
-api.locals.apiPath   = settings.apiPath;
-api.locals.userPath  = settings.userPath;
+/* Legacy code
+api.locals.apiPath = settings.apiPath;
+api.locals.userPath = settings.userPath;
 var apiServer = api.listen(settings.apiPort, function () {
-	let host = apiServer.address().address;
-	let port = apiServer.address().port;
-	console.log("[API] [Start] Listening at http://%s:%s", host, port);
+  let host = apiServer.address().address;
+  let port = apiServer.address().port;
+  console.log("[API] [Start] Listening at http://%s:%s", host, port);
 });
+ */
 
 // Setup and start the frontend page
-frontend.locals.apiPath   = settings.apiPath;
-frontend.locals.userPath  = settings.userPath;
+frontend.locals.apiPath = settings.apiPath;
+frontend.locals.userPath = settings.userPath;
 var frontendServer = frontend.listen(settings.userPort, function () {
-	let host = frontendServer.address().address;
-	let port = frontendServer.address().port;
-	console.log("[frontend] [Start] Listening at http://%s:%s", host, port);
+  let host = frontendServer.address().address;
+  let port = frontendServer.address().port;
+  console.log("[frontend] [Start] Listening at http://%s:%s", host, port);
 });
